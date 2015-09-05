c2c7a7b5ad32a7d1def895201f01cdc8ea0696a7
dfirstov <dfirstov@i-novus.ru>
2015-09-06 00:54:55 +0300

    solid-snake

---
+++ src/main/java/ru/firstquad/snake/SnakeGame.java
@@ -0,0 +1,12 @@
+package ru.firstquad.snake;
+
+/**
+ * Created by Dima on 05.09.2015.
+ */
+public class SnakeGame {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+
+    }
+}
